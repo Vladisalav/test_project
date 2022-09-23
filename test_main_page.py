8e92ca81d8d6e0072107ab7b7bc2a0bc1002acdd
Vlad_Ovcharenko <vladislav.ovcharenko110@gmail.com>
2022-09-24 02:21:17 +0300

    basic page

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,11 @@
 from selenium.webdriver.common.by import By
 
 
+def test_add_to_card(browser):
+    page = ProductPage(url="http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/, browseer")
+    url = browser.find_element_by_css_selector("#login_link")
+    link.click()
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+   browser.get(link)
+   go_to_login_page(browser)
