cf654e1367b8cb096048b3ec8d8920161703e5bf
Vlad_Ovcharenko <vladislav.ovcharenko110@gmail.com>
2022-09-25 14:21:51 +0300

    test_guest_can_go_to_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,8 @@
-from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 
 
-def test_add_to_card(browser):
-    page = ProductPage(url="http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/, browseer")
-    url = browser.find_element_by_css_selector("#login_link")
-    link.click()
-
-def test_guest_can_go_to_login_page(browser):
-   browser.get(link)
-   go_to_login_page(browser)
+def test_quest_can_go_to_login(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
