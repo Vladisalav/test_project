01a6ffc007d530182a8d8b6e45826c22ef3e173e
Vlad_Ovcharenko <vladislav.ovcharenko110@gmail.com>
2022-09-25 14:50:55 +0300

    is_element_present

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,16 @@
 from .pages.main_page import MainPage
 
 
-def test_quest_can_go_to_login(browser):
+# def test_quest_can_go_to_login(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.go_to_login_page()
+def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
+    page.should_be_login_link()
+
+
+
