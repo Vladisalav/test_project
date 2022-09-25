0938c9afca7f0b771df78fcc6de2c208544d2c28
Vlad_Ovcharenko <vladislav.ovcharenko110@gmail.com>
2022-09-25 16:00:10 +0300

    LoginPage

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,10 @@
-from .base_page import BasePage
-from selenium.webdriver.common.by import By
-
-class MainPage(BasePage):
-    # def should_be_login_link(self):
-    #     self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
-    def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+# from .base_page import BasePage
+# from .locators import LoginPageLocators
+#
+# class MainPage(BasePage):
+#     # def should_be_login_link(self):
+#     #     self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+#     # def should_be_login_link(self):
+#     #     assert self.is_element_present(By.CSS_SELECTOR, "#registration_link"), "Login link is not presented"
+#     def should_be_login_link(self):
+#         assert self.is_element_present(*LoginPageLocators.LOGIN_LINK), "Login link is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,4 @@
-from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 # def test_quest_can_go_to_login(browser):
@@ -7,10 +7,12 @@ from .pages.main_page import MainPage
 #     page.open()
 #     page.go_to_login_page()
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.should_be_login_url()
+    page.should_be_login_form()
+    page.should_be_register_form()
 
 
 
